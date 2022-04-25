25fd0e295bfcd2664dd6d22a16ae46423a3fa963
koki <kokis0327table@gmail.com>
2022-04-25 23:43:09 +0900

    初期　ファイル移動

---
+++ src/components/Mistake.js
@@ -0,0 +1,22 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+// function Mistake(props) {
+//   return <h2 className="mistake">{props.content}</h2>;
+// }
+
+// Mistake.propTypes = {
+//   content: PropTypes.string.isRequired
+// };
+
+// export default Mistake;
+
+function Mistake(props) {
+  return <h2 className="mistake">不正解！！</h2>;
+}
+
+Mistake.propTypes = {
+  content: PropTypes.string.isRequired,
+};
+
+export default Mistake;
