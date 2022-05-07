5f071de5dbc06ef8496a33e9a8070b2d766be9b9
koki <kokis0327table@gmail.com>
2022-05-07 23:21:40 +0900

    mistake css調整

--- src/components/Mistake.js
+++ src/components/Mistake.js
@@ -1,6 +1,7 @@
 import React from "react";
 import PropTypes from "prop-types";
 import { CSSTransitionGroup } from 'react-transition-group';
+import twitterImage from "../images/pages/index/return_btn_tw.png"; 
 
 
 
@@ -27,10 +28,27 @@ function Mistake(props) {
     >
       <div>
         <div class="mistake__heading">
-          <p>Bad luck!<br/><span class="animateion--neon">Third</span> stage<br/>{props.quizMistake}</p>
+          <p>Bad luck!
+            <br/><span class="animateion--neon">Third</span> stage <span class="text-logo--racket">🏓</span>
+            {/* <br/>{props.quizMistake} */}
+          </p>
         </div>
         <div class="mistake__body">
-          残念むねん残念むねん残念むねん残念むねん残念むねん残念むねん残念むねん残念むねん残念むねん残念むねん残念むねん残念むねん残念むねん残念むねん残念むねん
+          <p class="text">
+            残念むねん残念むねん残念むねん残念むねん残念むねん残念むねん残念むねん残念むねん残念むねん残念むねん残念むねん残念むねん残念むねん残念むねん残念むねん
+          </p>
+          <div class="appeal" data-appeal>
+              <div class="appeal__link" data-appeal-last>
+                <a href="https://twitter.com/Lobbing_?ref_src=twsrc%5Etfw">
+                  <img src={twitterImage} alt="Twitterに戻る"></img>
+                </a>
+              </div>
+              <div class="appeal__text">『リツイートとフォローもしてくれたら嬉しいです!!』</div>
+              
+              <div class="appeal__link appeal__link--return">
+                <a href="http://localhost:3000/">もう1度チャレンジする</a>
+              </div>
+            </div>
         </div>
       </div>
     </CSSTransitionGroup>
--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import * as serviceWorker from './serviceWorker';
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 
+
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
